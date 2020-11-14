1a5f3f468a4358f0ffee2cfcbd77ebf27145caec
Musa Danjuma <shamxeedmuhd@gmail.com>
2020-11-14 01:26:54 +0100

    Sweet Treat Section Added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyle';
 import Hero from './components/hero';
 import Product from './components/products';
-import { productData } from './components/products/Data';
+import { productData, productDataTwo } from './components/products/Data';
 import Feature from './components/feature';
 
 
@@ -13,6 +13,7 @@ function App() {
       <Hero />
       <Product heading='Choose Your Fave' data={productData} />
       <Feature />
+      <Product heading='Sweet Treats For You' data={productDataTwo} />
     </Router>
   );
 }
